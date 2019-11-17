1d13c1dcf9d94f4290e69bebd8c9cbb9fff50654
GOPALAKRISHNAN-R <gopalakrishnars94@gmail.com>
2019-11-17 21:37:29 +0530

    Initial commit

---
+++ HelloPython.py
@@ -0,0 +1,6 @@
+# This Python program prints Hello,Welcome to Python!! on console screen
+# @author R.Gopalakrishnan
+# @author gopalakrishnanr94@gmail.com
+# @author www.rgopalakrishnanmca.simplesite.com
+
+print("Hello,Welcome to Python!!")
